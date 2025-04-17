e4e9d70d79c71f4fd863464507865ba07db07d04
wiseCirno <78086755+wiseCirno@users.noreply.github.com>
2025-04-17 15:28:55 +0800

    :zap: different screen size definition

--- HRMSystemLiu.2025/tailwind.config.js
+++ HRMSystemLiu.2025/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
         "./**/*.{razor,cshtml}",
     ],
     theme: {
-        extend: {},
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
     },
     plugins: [
         require('@tailwindcss/forms'),
